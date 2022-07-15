bea02bb5b4dec66aef8e2f15a8b53053c72cf7f7
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-15 12:14:19 +0100

    btn reset/draw

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 const container = document.querySelector("#container");
 
-for (i = 1; i <= 16 * 16; i++) {
-  const content = document.createElement("div");
-  content.classList.add("box");
-  container.appendChild(content);
+const reset = document.querySelector(".btn-reset");
+
+let squares = 16;
+drawSquares(squares);
+
+function drawSquares(squares) {
+  for (i = 1; i <= squares * squares; i++) {
+    const content = document.createElement("div");
+    content.classList.add("box");
+    container.appendChild(content);
+  }
 }
 
-const box = document.querySelectorAll(".box");
+const boxes = document.querySelectorAll(".box");
 
-box.forEach(hoverBlack);
+boxes.forEach(hoverBlack);
 
-function hoverBlack(box) {
-  box.addEventListener("mouseover", () => {
-    box.classList.add("black");
+function hoverBlack(boxes) {
+  boxes.addEventListener("mouseover", () => {
+    boxes.classList.add("black");
   });
 }
+
+
+
+reset.addEventListener("click", () => {
+    document.querySelectorAll(".box").forEach(el => el.remove());
+  squares = prompt("Number of squares?");
+  drawSquares(squares);
+});
+
 // ** Alternative **
 // box.forEach(box => {
 //   box.addEventListener("mouseover", () => {
 //     box.classList.add("black");
 //   });
 // });
+
+//reset btn
