2776ea4906a7d22f3a0e36209cb0f34b8f13f9db
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-15 10:27:02 +0100

    hover effect

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
-content.classList.add("box");
-
-for (i = 1; i <= 16*16; i++) {
+for (i = 1; i <= 16 * 16; i++) {
   const content = document.createElement("div");
   content.classList.add("box");
   container.appendChild(content);
 }
+
+const box = document.querySelectorAll(".box");
+
+box.forEach(hoverBlack);
+
+function hoverBlack(box) {
+  box.addEventListener("mouseover", () => {
+    box.classList.add("black");
+  });
+}
+// ** Alternative **
+// box.forEach(box => {
+//   box.addEventListener("mouseover", () => {
+//     box.classList.add("black");
+//   });
+// });
