0c57ce93dc96e051d72a8aef6af22b706250b528
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-07-15 14:12:56 +0100

    btn funcionality added-adjusting css,code clean up

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
 const container = document.querySelector("#container");
-
 const reset = document.querySelector(".btn-reset");
 
 let squares = 16;
 drawSquares(squares);
 
+// add div's with class box
 function drawSquares(squares) {
   for (i = 1; i <= squares * squares; i++) {
     const content = document.createElement("div");
     content.classList.add("box");
     container.appendChild(content);
   }
+  // adjust css
+  container.style.cssText = `
+    display: inline-grid;
+    grid-template-columns: repeat(${squares}, auto)`;
+  //hover function added
+  const boxes = document.querySelectorAll(".box");
+  hover(boxes);
 }
-
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach(hoverBlack);
-
-function hoverBlack(boxes) {
-  boxes.addEventListener("mouseover", () => {
-    boxes.classList.add("black");
+//add class on hover
+function hover(boxes) {
+  boxes.forEach((boxes) => {
+    boxes.addEventListener("mouseover", () => {
+      boxes.classList.add("black");
+    });
   });
 }
 
-
-
+//reset btn
 reset.addEventListener("click", () => {
-    document.querySelectorAll(".box").forEach(el => el.remove());
+  document.querySelectorAll(".box").forEach((el) => el.remove());
+  //validation with escape
   squares = prompt("Number of squares?");
+  if (squares > 100) {
+    alert("Number of squares to high. Please insert number <100");
+    return;
+  }
   drawSquares(squares);
 });
 
 // ** Alternative **
-// box.forEach(box => {
-//   box.addEventListener("mouseover", () => {
-//     box.classList.add("black");
+
+// function hover(boxes) {
+//   boxes.addEventListener("mouseover", () => {
+//     boxes.classList.add("black");
 //   });
-// });
+// }
+// boxes.forEach(hover);
 
 //reset btn
